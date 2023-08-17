fed00fb5e56a9b2498829d32cdde72a0810aaa10
mukul <darshandc202@gmail.com>
2023-08-17 12:25:58 +0530

    change2

---
+++ 2.py
@@ -0,0 +1,6 @@
+import numpy as np
+a=np.array([[6,7,4],[2,5,6]])
+b=np.array([[2,5,6],[6,7,4]])
+print("adding the two matrix=",a+b)
+print("subtracting the two matrix=",a-b)
+print("multiplication of two matrix=",a*b)
